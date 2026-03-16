a52e32d6ba6e53797167ef3760cf76c6a42b592e
Naveen Nandyala <nandyalanaveenyadav11@gmail.com>
2026-03-16 16:16:29 +0530

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/css/custom.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     </head>
     <body class="app-body">
         <div class="app-wrapper">
